2c65c1f077ee741a71059a6b640a62d117330075
Munkhbayar <mgl0814@gmail.com>
2022-11-07 23:14:00 +0800

    error fix3

--- layout/MainLayoutEn.js
+++ layout/MainLayoutEn.js
@@ -1,11 +1,6 @@
 import Aboutexe from "../components/en/Aboutexe";
-import Domesticprojects from "../components/en/Domesticprojects";
 import Footer from "../components/en/Footer";
 import Navbar from "../components/en/Navbar";
-import Overseasprojects from "../components/en/Overseasprojects";
-import Projects from "../components/en/Projects";
-import Recruitment from "../components/en/Recruitment";
-import Sharedhouse from "../components/en/Sharedhouse";
 import Slider from "../components/en/Slider";
 
 
